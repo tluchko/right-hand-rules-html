ac1a5428ee468b5da81a34f499afe5a14c2bb0f4
T Luchko <tluchko@csun.edu>
2023-10-12 21:31:35 -0700

    added support for current on force RHR

--- images/force/generate_images.py
+++ images/force/generate_images.py
@@ -1,10 +1,12 @@
 import magneticrhr
 
-rhr = magneticrhr.MagneticRHR('particle')
-directions = list(rhr.directions.keys())
-directions.remove((0,0,0))
-for choice in ['magnetic force', 'magnetic field', 'particle']:
-    for direction1 in directions:
-        for direction2 in directions:
-            for charge in [-1, 1]:
-                rhr.display_problem(charge=charge, unknown_choice=choice, known1Vec=direction1, known2Vec=direction2)
+for charge_type in ['current']:  # , 'particle']
+    rhr = magneticrhr.MagneticRHR(charge_type=charge_type)
+    directions = list(rhr.directions.keys())
+    directions.remove((0, 0, 0))
+    for choice in ['magnetic force', 'magnetic field', 'particle']:
+        for direction1 in directions:
+            for direction2 in directions:
+                for charge in [-1, 1]:
+                    rhr.display_problem(
+                        charge=charge, unknown_choice=choice, known1Vec=direction1, known2Vec=direction2)
